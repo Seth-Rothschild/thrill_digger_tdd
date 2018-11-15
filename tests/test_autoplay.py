292f4d8c408da0c1ec4051ccfbbc3c5a5c697fc2
Seth-Rothschild <seth.j.rothschild@gmail.com>
2018-11-14 22:15:09 -0500

    Add option to set game difficulty in autoplay

--- tests/test_autoplay.py
+++ tests/test_autoplay.py
@@ -1,5 +1,5 @@
-from thrill_digger_tdd.board import Board
 from thrill_digger_tdd import autoplay
+
 
 def test_single_random_game():
     game = autoplay.single_random_game()
@@ -32,3 +32,29 @@ def test_strategy_kwarg():
             assert start[i][0] == loc[0]
             assert start[i][1] == loc[1]
         assert game.ruplist[-1] == 'boom' or game.game_won
+
+
+def test_single_game_params():
+    beg_game1 = autoplay.single_random_game(board='beginner')
+    beg_game2 = autoplay.single_simple_game(board='beginner')
+    int_game1 = autoplay.single_random_game(board='intermediate')
+    int_game2 = autoplay.single_simple_game(board='intermediate')
+    exp_game1 = autoplay.single_simple_game(board='expert')
+    exp_game2 = autoplay.single_simple_game(board='expert')
+
+    for game in [beg_game1, beg_game2]:
+        assert game.height == 4
+        assert game.width == 5
+        assert game.bombs == 4 - game.ruplist.count('boom')
+
+    for game in [int_game1, int_game2]:
+        dug_bombs = game.ruplist.count('boom') + game.ruplist.count('rupoor')
+        assert game.height == 5
+        assert game.width == 6
+        assert game.bombs == 8 - dug_bombs
+
+    for game in [exp_game1, exp_game2]:
+        dug_bombs = game.ruplist.count('boom') + game.ruplist.count('rupoor')
+        assert game.height == 5
+        assert game.width == 8
+        assert game.bombs == 16 - dug_bombs
--- thrill_digger_tdd/autoplay.py
+++ thrill_digger_tdd/autoplay.py
@@ -1,33 +1,48 @@
 from thrill_digger_tdd.board import Board
 
 
-def single_random_game():
-    play_area = Board()
-    play_area.game_start()
+def gen_game(game_type):
+    if game_type == 'beginner':
+        game = Board(4, 5)
+        game.game_start(4, 0)
+    if game_type == 'intermediate':
+        game = Board(5, 6)
+        game.game_start(4, 4)
+    if game_type == 'expert':
+        game = Board(5, 8)
+        game.game_start(8, 8)
+    return game
+
+
+def single_random_game(board='expert'):
+    play_area = gen_game(board)
     while not play_area.game_over:
         play_area.random_play()
     return play_area
 
 
-def single_simple_game():
-    play_area = Board()
-    play_area.game_start()
+def single_simple_game(board='expert'):
+    play_area = gen_game(board)
     move_count = 0
-    start = [(3, 1), (3, 3), (3, 5), (1, 1), (1, 3), (1, 5)]
-    while not play_area.game_over:
-        if move_count < 6:
-            play_area.dig(start[move_count])
-        else:
-            play_area.random_play()
-        move_count += 1
+    if board == 'expert':
+        start = [(3, 1), (3, 3), (3, 5), (1, 1), (1, 3), (1, 5)]
+
+        while not play_area.game_over:
+            if move_count < 6:
+                play_area.dig(start[move_count])
+            else:
+                play_area.random_play()
+            move_count += 1
+    else:
+        play_area = single_random_game(board)
     return play_area
 
-def autoplay(num_games, strategy='random'):
+
+def autoplay(num_games, strategy='random', board='expert'):
     gamelist = []
     for i in range(num_games):
         if strategy == 'random':
-            gamelist.append(single_random_game())
+            gamelist.append(single_random_game(board))
         if strategy == 'simple':
-            gamelist.append(single_simple_game())
+            gamelist.append(single_simple_game(board))
     return gamelist
-        
